06097c7cc633558cc74e55dbd90be8900f06f263
dtjah <123922977+dtjah@users.noreply.github.com>
2023-02-08 22:27:27 -0500

    Update analyze_and_send_email.py

--- analyze_and_send_email.py
+++ analyze_and_send_email.py
@@ -25,7 +25,7 @@ def main():
     data = response.json()
     new_commit = data[0]["sha"]
     
-    recipient = "dtjahjadi1@babson.edu"
+    recipient = "dtjahjadi1@babson.edu" , "davidst611@gmail.com"
     message = f"The repository {repo_name} has been updated with a new commit: {new_commit}\n\nBest regards,\nDavid Setiawan Tjahjadi"
     send_email("Github repository update", message, recipient)
 
